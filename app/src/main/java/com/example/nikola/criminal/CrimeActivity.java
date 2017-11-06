c8c08224c54701a4635b7d3a8e00b0f6f9217405
joysoi <nikola.joysoi@gmail.com>
2017-11-05 22:33:37 -0500

    CrimApp

---
+++ app/src/main/java/com/example/nikola/criminal/CrimeActivity.java
@@ -0,0 +1,11 @@
+package com.example.nikola.criminal;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeFragment();
+    }
+}
