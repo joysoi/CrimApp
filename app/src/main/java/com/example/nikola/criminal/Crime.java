c7451a170e0a7e9faaca9b89dba62ac1561013cf
joysoi <nikola.joysoi@gmail.com>
2017-11-13 11:16:13 -0500

    implementing parcelable

--- app/src/main/java/com/example/nikola/criminal/Crime.java
+++ app/src/main/java/com/example/nikola/criminal/Crime.java
@@ -1,9 +1,12 @@
 package com.example.nikola.criminal;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Date;
 import java.util.UUID;
 
-public class Crime {
+public class Crime implements Parcelable {
 
     private UUID mID;
     private String mTitle;
@@ -51,4 +54,39 @@ public class Crime {
     public void setRequiredPolice(boolean requiredPolice) {
         mRequiredPolice = requiredPolice;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeSerializable(this.mID);
+        dest.writeString(this.mTitle);
+        dest.writeLong(this.mDate != null ? this.mDate.getTime() : -1);
+        dest.writeByte(this.mSolved ? (byte) 1 : (byte) 0);
+        dest.writeByte(this.mRequiredPolice ? (byte) 1 : (byte) 0);
+    }
+
+    protected Crime(Parcel in) {
+        this.mID = (UUID) in.readSerializable();
+        this.mTitle = in.readString();
+        long tmpMDate = in.readLong();
+        this.mDate = tmpMDate == -1 ? null : new Date(tmpMDate);
+        this.mSolved = in.readByte() != 0;
+        this.mRequiredPolice = in.readByte() != 0;
+    }
+
+    public static final Parcelable.Creator<Crime> CREATOR = new Parcelable.Creator<Crime>() {
+        @Override
+        public Crime createFromParcel(Parcel source) {
+            return new Crime(source);
+        }
+
+        @Override
+        public Crime[] newArray(int size) {
+            return new Crime[size];
+        }
+    };
 }
