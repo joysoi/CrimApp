2e9e297fd2d320627ad34481b6e542cd5ad63e08
joysoi <nikola.joysoi@gmail.com>
2017-11-27 17:37:21 -0500

    better for loop performance

--- app/src/main/java/com/example/nikola/criminal/CrimeLab.java
+++ app/src/main/java/com/example/nikola/criminal/CrimeLab.java
@@ -3,16 +3,17 @@ package com.example.nikola.criminal;
 import android.content.Context;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 
 public class CrimeLab {
 
     private static CrimeLab sCrimeLab;
-    private List<Crime> mCrimes;
-    Crime crime = new Crime();
-//    LinkedHashMap<UUID, Crime> hashMap = new LinkedHashMap();
+
+    private Map<UUID, Crime> mCrimes;
 
     static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
@@ -22,41 +23,24 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context) {
-        mCrimes = new ArrayList<>();
+        mCrimes = new LinkedHashMap<>();
         for (int i = 0; i < 100; i++) {
-            crime = new Crime();
+            Crime crime = new Crime();
             crime.setTitle("Crime #" + " " + i);
             crime.setSolved(i % 2 == 0);
-            crime.setRequiredPolice(i % 3 == 0);
-//            hashMap.put(crime.getID(), crime);
-            mCrimes.add(crime);
+//            crime.setRequiredPolice(i % 3 == 0);
+            mCrimes.put(crime.getID(), crime);
+
         }
     }
 
     List<Crime> getCrimes() {
-        return mCrimes;
+
+        return new ArrayList<>(mCrimes.values());
     }
 
 
-//    public Crime getCrime(UUID id, int idPos) {
-//        if (mCrimes.size() == idPos) {
-//            if (crime.getID().equals(id)){
-//                return crime;
-//            }else {
-//                return null;
-//            }
-//        }
-//        return null;
-//    }
-//}
-
     public Crime getCrime(UUID id) {
-
-        for (Crime crime : mCrimes) {
-            if (crime.getID().equals(id)) {
-                return crime;
-            }
-        }
-        return null;
+        return mCrimes.get(id);
     }
 }
