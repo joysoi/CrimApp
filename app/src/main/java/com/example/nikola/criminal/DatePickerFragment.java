8345b2315e36a26a81b06e2818f793f662193640
joysoi <nikola.joysoi@gmail.com>
2017-12-05 23:54:51 -0500

    modyfing date picker fragment

--- app/src/main/java/com/example/nikola/criminal/DatePickerFragment.java
+++ app/src/main/java/com/example/nikola/criminal/DatePickerFragment.java
@@ -2,15 +2,14 @@ package com.example.nikola.criminal;
 
 
 import android.app.Activity;
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
-import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
 
 import java.util.Calendar;
@@ -34,11 +33,13 @@ public class DatePickerFragment extends DialogFragment {
         return fragment;
     }
 
-    @NonNull
+    @Nullable
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
-        final Date date = (Date) getArguments().getSerializable(ARG_DATE);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.dialog_date, null);
+        Date date = (Date) getArguments().getSerializable(ARG_DATE);
+
 
         final Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
@@ -50,33 +51,30 @@ public class DatePickerFragment extends DialogFragment {
         mDatePicker = v.findViewById(R.id.dialog_date_picker);
         mDatePicker.init(year, month, day, null);
 
-
-        return new AlertDialog.Builder(getActivity())
-                .setView(v)
-                .setTitle(R.string.date_picker_title)
-                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int which) {
-                        int year = mDatePicker.getYear();
-                        int month = mDatePicker.getMonth();
-                        int day = mDatePicker.getDayOfMonth();
-                        Date date1 = new GregorianCalendar(year, month, day).getTime();
-                        sendResult(Activity.RESULT_OK, date1);
-                    }
-                })
-                .create();
+        Button mOkBtn = v.findViewById(R.id.okBtn);
+        mOkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int year = mDatePicker.getYear();
+                int month = mDatePicker.getMonth();
+                int day = mDatePicker.getDayOfMonth();
+                Date datePicker = new GregorianCalendar(year, month, day).getTime();
+                sendResult(Activity.RESULT_OK, datePicker);
+                getActivity().finish();
+            }
+        });
+        return v;
     }
 
     private void sendResult(int resultCode, Date date) {
 
-        if (getTargetFragment() == null) {
-            return;
-        }
-
         Intent intent = new Intent();
         intent.putExtra(EXTRA_DATE, date);
-        getTargetFragment().onActivityResult(getTargetRequestCode(), resultCode, intent);
 
+        if (getTargetFragment() == null) {
+            getActivity().setResult(resultCode, intent);
+        } else {
+            getTargetFragment().onActivityResult(getTargetRequestCode(), resultCode, intent);
+        }
     }
-
 }
