ebb823439d54aadfc9633349154f8f471d4cec05
joysoi <nikola.joysoi@gmail.com>
2018-01-12 13:36:55 -0500

    implemented launcher activity

---
+++ app/src/main/java/com/example/nikola/criminal/App.java
@@ -0,0 +1,32 @@
+package com.example.nikola.criminal;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.example.nikola.criminal.database.DbHelper;
+import com.facebook.stetho.Stetho;
+
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        DbHelper.getInstance(getApplicationContext()).init(this);
+        stethoInit(this);
+    }
+
+    private void stethoInit(Context context) {
+        Stetho.InitializerBuilder initializerBuilder =
+                Stetho.newInitializerBuilder(context);
+        initializerBuilder.enableWebKitInspector(
+                Stetho.defaultInspectorModulesProvider(context)
+        );
+        initializerBuilder.enableDumpapp(
+                Stetho.defaultDumperPluginsProvider(context)
+        );
+        Stetho.Initializer initializer = initializerBuilder.build();
+        Stetho.initialize(initializer);
+    }
+}
+
