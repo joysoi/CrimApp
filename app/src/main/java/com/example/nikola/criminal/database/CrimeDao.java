f4e24fcad3f55d3e7a734095c8789f4d4bcc09d2
joysoi <nikola.joysoi@gmail.com>
2018-01-24 11:25:01 -0500

    add Single type getAllCrimes

--- app/src/main/java/com/example/nikola/criminal/database/CrimeDao.java
+++ app/src/main/java/com/example/nikola/criminal/database/CrimeDao.java
@@ -4,23 +4,28 @@ package com.example.nikola.criminal.database;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 
 import java.util.List;
+import java.util.UUID;
+
+import io.reactivex.Single;
 
 @Dao
 public interface CrimeDao {
 
     @Query("SELECT * FROM Crime")
-    List<Crime> getAllCrimes();
+    Single<List<Crime>> getAllCrimes();
 
-    @Query("SELECT * FROM Crime WHERE id = :id")
-    Crime getCrimeById(int id);
+    @Query("SELECT * FROM crime WHERE id LIKE :id")
+    Single<Crime> getCrimeById(UUID id);
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertCrime(Crime crime);
 
     @Delete
     void deleteCrime(Crime crime);
 
 }
+
