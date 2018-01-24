a9d0942d25cd6fdaf8092de42f54b99b0a419869
joysoi <nikola.joysoi@gmail.com>
2018-01-24 11:23:56 -0500

    add UUID converter

--- app/src/main/java/com/example/nikola/criminal/database/AppDatabase.java
+++ app/src/main/java/com/example/nikola/criminal/database/AppDatabase.java
@@ -4,8 +4,8 @@ import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 import android.arch.persistence.room.TypeConverters;
 
-@Database(entities = {Crime.class}, version = 1)
-@TypeConverters({DateConverter.class})
+@Database(entities = {Crime.class}, version = 1, exportSchema = false)
+@TypeConverters({DateConverter.class, UUIDConverter.class})
 public abstract class AppDatabase extends RoomDatabase {
     public abstract CrimeDao mCrimeDao();
 }
