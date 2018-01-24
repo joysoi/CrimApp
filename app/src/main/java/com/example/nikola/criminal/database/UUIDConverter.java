8f21c6b599c86eedf21f5fa4e216f738d11c06ff
joysoi <nikola.joysoi@gmail.com>
2018-01-24 11:25:49 -0500

    create UUID Converter to Long and vise versa

--- app/src/main/java/com/example/nikola/criminal/database/UUIDConverter.java
+++ app/src/main/java/com/example/nikola/criminal/database/UUIDConverter.java
@@ -1,22 +1,19 @@
-//package com.example.nikola.criminal.database;
-//
-//import android.animation.TypeConverter;
-//import android.os.Build;
-//import android.support.annotation.RequiresApi;
-//
-//import java.util.UUID;
-//
-//
-//@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-//public class UUIDConverter extends TypeConverter<Integer, UUID> {
-//
-//
-//    public UUIDConverter(Class<Integer> fromClass, Class<UUID> toClass) {
-//        super(fromClass, toClass);
-//    }
-//
-//    @Override
-//    public UUID convert(Integer integer) {
-//       return integer == null ? null : UUID.fromString(integer);
-//    }
-//}
+package com.example.nikola.criminal.database;
+
+
+import android.arch.persistence.room.TypeConverter;
+
+import java.util.UUID;
+
+public class UUIDConverter {
+
+    @TypeConverter
+    public static UUID toUUID(Long value) {
+        return value == null ? null : new UUID(value, value);
+    }
+
+    @TypeConverter
+    public static Long toLong(UUID value) {
+        return value == null ? null : value.getMostSignificantBits();
+    }
+}
